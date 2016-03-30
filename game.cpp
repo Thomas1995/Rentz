71fbe31cb6d8a36f7f8801210a01fda2a0abab8c
Thomas66 <suthcr@yahoo.com>
2016-03-30 09:08:26 +0300

    game class minor changes

--- game.cpp
+++ game.cpp
@@ -12,15 +12,29 @@ void Game::Start() {
     G.players.push_back(new Bot_Thomas());
     G.players.push_back(new Bot_Thomas());
 
-    G.PlayKingOfHearts();
+    G.firstPlayer = G.players.begin();
+
+    G.PlayRound();
 }
 
-void Game::PlayKingOfHearts() {
-    std::stack<Card> st;
-    for(auto player : players) {
-        Card playedCard = player->PlayCard(st);
-        st.push(playedCard);
-    }
+void Game::PlayerAction(Bot* const player) {
+    Card playedCard = player->PlayCard(cardStack);
+    cardStack.push(playedCard);
+    player->RemoveCard(playedCard);
+}
+
+void Game::IterateThroughPlayers(std::vector<Bot*>::iterator iterator) {
+    while(!cardStack.empty())
+      cardStack.pop();
+
+    for(auto it = iterator; it != players.end(); ++it)
+        PlayerAction(*it);
+    for(auto it = players.begin(); it != iterator; ++it)
+        PlayerAction(*it);
+}
+
+void Game::PlayRound() {
+    IterateThroughPlayers(firstPlayer);
 }
 
 Game::~Game() {
--- game.h
+++ game.h
@@ -2,21 +2,28 @@
 #define GAME_H
 
 #include "util/require.h"
+#include "util/card.h"
 #include "bots/bot.h"
 
 #include "bots/bot_Thomas.cpp"
 
 #include <vector>
+#include <stack>
 
 class Game {
     std::vector<Bot*> players;
+    std::vector<Bot*>::iterator firstPlayer;
+    std::stack<Card> cardStack;
 
     Game();
+
+    void IterateThroughPlayers(std::vector<Bot*>::iterator iterator);
+    void PlayerAction(Bot* const player);
 
 public:
     static void Start();
 
-    void PlayKingOfHearts();
+    void PlayRound();
 
     ~Game();
 };
