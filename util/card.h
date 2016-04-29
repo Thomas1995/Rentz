8e2dc6c9657cc8ccdb0c3cc00f9ab7d54e71b3ce
Eric Peter Stavarache <ericptst@gmail.com>
2016-04-29 11:26:17 +0300

    Added const modifiers to card

--- util/card.h
+++ util/card.h
@@ -6,32 +6,30 @@
 #include <algorithm>
 #include <assert.h>
 
-class Card {
+struct Card {
 
-int value;
-/*
-    2-10
-    J: 11
-    Q: 12
-    K: 13
-    A: 14
+  int value;
+  /*
+     2-10
+J: 11
+Q: 12
+K: 13
+A: 14
 */
 
-char suite;
-/*
-    Hearts: 'H'
-    Clubs: 'C'
-    Diamonds: 'D'
-    Spades: 'S'
+  char suite;
+  /*
+Hearts: 'H'
+Clubs: 'C'
+Diamonds: 'D'
+Spades: 'S'
 */
 
-static char suites[];
+  static char suites[];
 
-std::string to_string() const;
+  std::string to_string() const;
 
-public:
-
-  int suiteIndex() {
+  int suiteIndex() const {
     for(int i = 0; i < 4; ++i) {
       if(suites[i] == suite)
         return i;
@@ -39,7 +37,7 @@ public:
     assert(false);
   }
 
-  uint8_t encode() {
+  uint8_t encode() const {
     uint8_t ret = value;
     //the last 4 bits represent the value
     ret |= suiteIndex() << 4;
@@ -47,23 +45,23 @@ public:
     return ret;
   }
 
-    Card(uint8_t code);
-    Card(int value, char suite);
-    Card(const Card& c);
+  Card(uint8_t code);
+  Card(int value, char suite);
+  Card(const Card& c);
 
-    bool operator == (const Card& c) const;
+  bool operator == (const Card& c) const;
 
-    bool isBeatenBy(const Card& c) const;
-    bool isSameSuite(const Card& c) const;
+  bool isBeatenBy(const Card& c) const;
+  bool isSameSuite(const Card& c) const;
 
-    int getValue() const;
-    char getSuite() const;
+  int getValue() const;
+  char getSuite() const;
 
-    static std::vector<Card> getAllCards();
+  static std::vector<Card> getAllCards();
 
-    friend std::ostream& operator << (std::ostream& os, const Card& c);
+  friend std::ostream& operator << (std::ostream& os, const Card& c);
 
-    static int lowestCard;
+  static int lowestCard;
 };
 
 #endif // CARD_H
